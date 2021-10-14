1db9953b7806729386c5481ba8d15efa5c61e96d
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-14 20:50:05 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle tupoy = new Angle(120,-13,-8);
+            Console.WriteLine("Угол: {0} градусов, {1} минут, {2} секунд.", tupoy.Gradus, tupoy.Min, tupoy.Sec);
+            tupoy.ToRadians();
+            Angle pryamoy = new Angle(90);
+            Console.WriteLine("Угол: {0} градусов, {1} минут, {2} секунд.", pryamoy.Gradus, pryamoy.Min, pryamoy.Sec);
+            pryamoy.ToRadians();
+            Console.ReadKey();
+        }
+    }
+
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus
+        {
+            set
+            {
+                if (value >= 0 && value <= 360)
+                {
+                    this.gradus = value;
+                }
+                else
+                {
+                    Console.WriteLine("Градус должен быть в диапазоне 0-360");
+                }
+            }
+            get
+            {
+                return gradus;
+            }
+        }
+        public int Min
+        {
+            set
+            {
+                if (value >= 0 && value < 60)
+                {
+                    this.min = value;
+                }
+                else
+                {
+                    Console.WriteLine("Минуты должны быть в диапазоне 0-59");
+                }
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public int Sec
+        {
+            set
+            {
+                if (value >= 0 && value < 60)
+                {
+                    this.sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("Секунды должны быть в диапазоне 0-59");
+                }
+            }
+            get
+            {
+                return sec;
+            }
+        }
+
+        public Angle (int gradus, int min=0, int sec=0)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public void ToRadians()
+        {
+            double radian = (gradus + (min / 60) + (sec / 60)) * Math.PI / 180;
+            Console.WriteLine("Угол в радианах равен: {0:f2}", radian);
+        }
+    }
+}
